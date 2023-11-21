0029c65a5b39b83159108c96076dda3942db6ea5
isetbz <isetb@iset.com>
2023-11-21 12:34:21 +0000

    api

---
+++ app/src/main/java/com/example/marsphotos/api/MarsApiService.kt
@@ -0,0 +1,26 @@
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    @GET("photos")
+    fun getPhotos(): String
+}
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
